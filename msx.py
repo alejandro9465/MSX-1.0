e68217a9990c7a9189f295b703acacb8b4821832
alejandro9465 <alejandrogp9465@gmail.com>
2024-11-27 22:25:33 -0300

    Update msx.py

--- msx.py
+++ msx.py
@@ -1,4 +1,6 @@
-# MSX 1.0
+# MSX 1.0.1
+
+# Version minecraft desde 1.7.10 hasta 1.21.3
 
 # 1. Instala la extensión de Python
 # 2. Click al botón de arriba a la derecha para iniciar el script [▶]
@@ -6,19 +8,17 @@
 # Si no aparece el botón, reinicia la página o cambia de navegador.
 
 # Github da 120 horas sin embargo las horas estan divididas entre la cantidad de nucleos.
-# Si seleccionamos el plan de 4 cores serian 30 horas
-# Si seleccionamos el plan de 2 cores serian 60 horas
-# El script ya aguanta tener 240 minutos (4 horas) de inactividad
+# El plan de 4 cores serian 30 horas
+# El plan de 2 cores serian 60 horas
+# El script ya aguanta tener 240 minutos (4 horas) de inactividad.
 # Apagar el servidor al no usarlo para prolongar las horas.
 
-# Para ver las horas que te quedan, ve al siguiente link y dale a cerrar o a la [X]
-# Luego baja donde dice codespaces y te dira cuantas horas usaste/quedan.
+# Para ver las horas que te quedan, ve al siguiente link y dale a cerrar, o a la [X] Luego baja donde dice codespaces y te dira cuantas horas usaste/quedan.
 # https://github.com/settings/billing/summary?open_metered_usage_report=true#usage
 
+# Recomendado usar [playit] como servicio de ip.
 
-# Desde 1.7.10 hasta 1.21.3
-
-
+# Si el servidor no inicia prueba cambiando el plan de cores a 4
 
 
 
